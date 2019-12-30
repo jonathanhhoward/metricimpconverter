5e50c0bce551b619c4a47a3d1c87fc41f5d12ad8
slipskid <2773262@gmail.com>
2019-12-30 12:35:01 -0500

    Format to Standard

--- server.js
+++ server.js
@@ -1,57 +1,57 @@
-'use strict';
+'use strict'
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+var express = require('express')
+var bodyParser = require('body-parser')
+var expect = require('chai').expect
+var cors = require('cors')
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+var apiRoutes = require('./routes/api.js')
+var fccTestingRoutes = require('./routes/fcctesting.js')
+var runner = require('./test-runner')
 
-var app = express();
+var app = express()
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })) //For FCC testing purposes only
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+    res.sendFile(process.cwd() + '/views/index.html')
+  })
 
 //For FCC testing purposes
-fccTestingRoutes(app);
+fccTestingRoutes(app)
 
 //Routing for API
-apiRoutes(app);
+apiRoutes(app)
 
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
-    .send('Not Found');
-});
+    .send('Not Found')
+})
 
 //Start our server and tests!
 const server = app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + server.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  console.log('Listening on port ' + server.address().port)
+  if (process.env.NODE_ENV === 'test') {
+    console.log('Running Tests...')
     setTimeout(function () {
       try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        runner.run()
+      } catch (e) {
+        var error = e
+        console.log('Tests are not valid:')
+        console.log(error)
       }
-    }, 1500);
+    }, 1500)
   }
-});
+})
 
-module.exports = app; //for testing
+module.exports = app //for testing
