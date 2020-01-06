5f1fb9fca244c27ccf0143b93d92b4683aed204d
slipskid <2773262@gmail.com>
2020-01-06 18:08:11 -0500

    Handle convert 10L

--- routes/api.js
+++ routes/api.js
@@ -25,7 +25,7 @@ module.exports = function (app) {
       const toString = convertHandler.getString(initNum, initUnit, returnNum,
         returnUnit)
 
-      //res.json
+      res.json({initNum, initUnit, returnNum, returnUnit, toString})
     })
 
 }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,9 +16,9 @@ chai.use(chaiHttp);
 suite('Functional Tests', function() {
 
   suite('Routing Tests', function() {
-    
+
     suite('GET /api/convert => conversion object', function() {
-      
+
       test('Convert 10L (valid input)', function(done) {
        chai.request(server)
         .get('/api/convert')
@@ -26,33 +26,33 @@ suite('Functional Tests', function() {
         .end(function(err, res){
           assert.equal(res.status, 200);
           assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
+          assert.equal(res.body.initUnit, 'l');
           assert.approximately(res.body.returnNum, 2.64172, 0.1);
           assert.equal(res.body.returnUnit, 'gal');
           done();
         });
       });
-      
+
       test('Convert 32g (invalid input unit)', function(done) {
-        
+
         //done();
       });
-      
+
       test('Convert 3/7.2/4kg (invalid number)', function(done) {
-        
+
         //done();
-      });  
-      
+      });
+
       test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
-        
+
         //done();
       });
-      
+
       test('Convert kg (no number)', function(done) {
-        
+
         //done();
       });
-      
+
     });
 
   });
